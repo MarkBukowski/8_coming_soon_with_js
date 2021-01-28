76171c6b375ffe13e61c04bb9784f66b375adc82
MarkBukowski <MarkBukowski@users.noreply.github.com>
2021-01-29 01:19:44 +0200

    Render clock  #10

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,6 +1,11 @@
 function clock(selector) {
+    // 
+
+
     const DOM = document.querySelector(selector);
-    const numbers = [432, 9, 37, 39]
+    let allValuesDOM = null;
+    const deadline = '01-04 14:00:00';
+    let numbers = calcTime(deadline);
     const labels = ['days', 'hours', 'minutes', 'seconds'];
     let HTML = '';
 
@@ -12,7 +17,16 @@ function clock(selector) {
     }
 
     DOM.innerHTML = HTML;
+    allValuesDOM = document.querySelectorAll(`${selector} .value`);
+
+    setInterval(function () {
+        numbers = calcTime(deadline);
+        for (let i = 0; i < 4; i++) {
+            allValuesDOM[i].innerText = numberFormat(numbers[i]);
+        }
+    }, 1000)
 }
+
 
 function numberFormat(number) {
     if (number < 10) {
@@ -21,4 +35,61 @@ function numberFormat(number) {
     return number;
 }
 
+
+function calcTime(deadline) {
+    const date = new Date();
+    const now = Date.now();
+    let year = date.getFullYear();
+    let fullDeadline = `${year}-${deadline}`;
+    let fullDlMs = (new Date(fullDeadline)).getTime();
+
+    if (fullDlMs < now) {
+        year++;
+        fullDeadline = `${year}-${deadline}`;
+        fullDlMs = (new Date(fullDeadline)).getTime();
+    }
+
+    const diff = fullDlMs - now;
+    const seconds = Math.round(diff / 1000);
+
+    const days = Math.floor(seconds / 60 / 60 / 24);
+    let unusedSec = (seconds - days * 60 * 60 * 24);
+    const hours = Math.floor(unusedSec / 60 / 60);
+    unusedSec -= hours * 60 * 60;
+
+    const minutes = Math.floor(unusedSec / 60);
+    unusedSec -= minutes * 60;
+
+    return [days, hours, minutes, unusedSec];
+}
+
+
 export { clock }
+
+
+
+
+// Reikalavimai:
+// - rasti vieta
+// - istraukti skaiciu
+// - mazint kas sekunde iki 0
+// - pasiekus 0 rasyt finish
+
+
+// function counter(selector) {
+//     const DOM = document.querySelector(selector);
+//     const number = parseInt(DOM.innterText);
+
+//     const timer = setInterval(function () {
+//         DOM.innterText = number;
+//         number--;
+//         if (number < 0) {
+//             clearInterval(timer);
+//             DOM.innterText = 'finish';
+//         }
+//     }, 1000);
+
+// }
+
+
+// export { counter }
